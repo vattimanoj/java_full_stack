2578b51c08609f454e5f282d5339ba84324f7630
vattimanoj <manojvatti2004@gmail.com>
2025-06-20 12:23:43 +0530

    StarSquare.java

--- StarSquare.java
+++ StarSquare.java
@@ -212,7 +212,7 @@ System.out.println();
 }}}*/
 
 
-import java.util.Scanner;
+/*import java.util.Scanner;
  class Pattern7
  {
 public static void main (String[] args)
@@ -244,10 +244,223 @@ System.out.print(" *");
 System.out.println();
 }
 System.out.println();
-}}}
+}}}*/
 
 
+/*import java.util.Scanner;
+ class StarDiamond {
+    public static void main(String[] args) {
+      int n;
+Scanner s=new Scanner(System.in);
+System.out.println("enter a value");
+n=s.nextInt();
 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}*/
+
+/*import java.util.Scanner;
+class HollowDiamond {
+    public static void main(String[] args) {
+
+   int n;
+Scanner s=new Scanner(System.in);
+System.out.println("enter a value");
+n=s.nextInt();
+
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}*/
+
+
+ 
+/*class pattren14 {
+public static void main(String args[]){
+int n;
+Scanner s1=new Scanner(System.in);
+System.out.println("Enter the value:");
+n=s1.nextInt();
+for(int i=0;i<n;i++) {
+for(int j=0;j<n;j++) {
+if(i==0 || j==0 || j==n-1 || i==n-1) {
+System.out.print("*");
+}
+else {
+System.out.print(" ");
+}
+}
+System.out.println();
+}
+}
+}*/
+
+/*class Pattren15 {
+public static void main(String args[]){
+int n;
+Scanner s1=new Scanner(System.in);
+System.out.println("Enter the value:");
+n=s1.nextInt();
+for(int i=0;i<n;i++) {
+for(int j=0;j<n;j++) {
+if(i==n/2 || j==n/2) {
+System.out.print("*");
+}
+else {
+System.out.print(" ");
+}
+}
+System.out.println();
+}
+}
+}*/
+
+/*class Pattren16 {
+public static void main(String args[]){
+int n;
+Scanner s1=new Scanner(System.in);
+System.out.println("Enter the value:");
+n=s1.nextInt();
+for(int i=1;i<=n;i++) {
+for(int j=1;j<=n;j++) {
+if(i==j || j==n-i+1) {
+System.out.print("*");
+}
+else {
+System.out.print(" ");
+}
+}
+System.out.println();
+}
+}
+}*/
+
+/*class Pattren17 {
+public static void main(String args[]){
+int n;
+Scanner s1=new Scanner(System.in);
+System.out.println("Enter the value:");
+n=s1.nextInt();
+for(int i=0;i<n;i++) {
+for(int j=0;j<n;j++) {
+if(i==n/2 || j==n/2 || i==0 || j==0 || j==n-1 || i==n-1) {
+System.out.print("*");
+}
+else {
+System.out.print(" ");
+}
+}
+System.out.println();
+}
+}
+}*/
+
+/*class Pattren18 {
+public static void main(String args[]){
+int n;
+Scanner s1=new Scanner(System.in);
+System.out.println("Enter the value:");
+n=s1.nextInt();
+for(int i=n;i>0;i--) {
+for(int j=0;j<i;j++) {
+if(j==0 || i==n || j==i-1) {
+System.out.print("*");
+}
+else {
+System.out.print(" ");
+}
+}
+System.out.println();
+}
+}
+}*/
+
+/*class Pattren19 {
+public static void main(String args[]){
+int n;
+Scanner s1=new Scanner(System.in);
+System.out.println("Enter the value:");
+n=s1.nextInt();
+for(int i=0;i<=n;i++) {
+for(int j=0;j<=i;j++) {
+if(j!=i) {
+System.out.print("*");
+}
+else {
+System.out.print(j);
+}
+}
+System.out.println();
+}
+}
+}*/
+
+/*class Pattren20 {
+public static void main(String args[]){
+int n;
+Scanner s1=new Scanner(System.in);
+System.out.println("Enter the value:");
+n=s1.nextInt();
+for(int i=0;i<=n;i++) {
+for(int j=0;j<=n;j++) {
+if(j==0 || i==0 || i==j) {
+System.out.print("*");
+}
+else {
+System.out.print(" ");
+}
+}
+System.out.println();
+}
+}
+}*/
 
 
 
